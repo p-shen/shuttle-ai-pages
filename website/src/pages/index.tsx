7dec618c89e3465b7a9769932c8efbbe62f8a748
p-shen <peterzhuo.shen@gmail.com>
2024-07-03 14:50:34 -0700

    Change theme

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Shuttle AI Biomedical
           </Link>
         </div>
       </div>
